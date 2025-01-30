62d701f1dd71cc862afc5cba8fb6b67f70818858
Luka Dojcinovic <56648891+Luka-D@users.noreply.github.com>
2025-01-30 17:44:54 -0500

    False import to fail
    Will purposely fail

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Purpose will fail
+import not_a_library
+
 # Standard
 from typing import Callable, Dict, List, Optional, Union
 import dataclasses
